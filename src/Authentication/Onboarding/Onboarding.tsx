9b25acf9f31578cbd0e764712822de1488e361d3
Leonam de Paula <leonamjlpaula@gmail.com>
2020-11-11 12:05:50 -0300

    First Commit

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, StyleSheet, ScrollView, Dimensions } from "react-native";
+
+import Slide, { SLIDE_HEIGHT } from "./Slide";
+
+const { width, height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  slider: {
+    height: SLIDE_HEIGHT,
+    backgroundColor: "cyan",
+    borderBottomRightRadius: 75,
+  },
+  footer: {
+    flex: 1,
+  },
+});
+
+const Onboarding = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.slider}>
+        <ScrollView
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+        >
+          <Slide label="Relaxed" />
+          <Slide label="Playful" right />
+          <Slide label="Excentric" />
+          <Slide label="Funky" right />
+        </ScrollView>
+      </View>
+      <View style={styles.footer}>
+        <View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor: "cyan" }}
+        />
+        <View
+          style={{ flex: 1, backgroundColor: "white", borderTopLeftRadius: 75 }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Onboarding;
